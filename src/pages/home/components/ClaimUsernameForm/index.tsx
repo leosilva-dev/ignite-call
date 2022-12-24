8feea02064da114bf45de8554ac9e7f50ed086d8
Leonardo Silva <silvaleonardo.ti@gmail.com>
2022-12-24 00:43:33 -0300

    add ClaimUsername form

---
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -0,0 +1,14 @@
+import { TextInput, Button } from "@ignite-ui/react";
+import { ArrowRight } from "phosphor-react";
+import { Form } from "./styles";
+
+export function ClaimUsernameForm() {
+  return (
+    <Form as="form">
+      <TextInput size={"sm"} prefix="ignite.com/" placeholder="seu-usuario" />
+      <Button size={"sm"} type="submit">
+        Reservar <ArrowRight />
+      </Button>
+    </Form>
+  );
+}
