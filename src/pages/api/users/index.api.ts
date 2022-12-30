72e6d4eb1f918e39767f2f27e549d19417aa37f3
Leonardo Silva <silvaleonardo.ti@gmail.com>
2022-12-30 01:20:37 -0300

    validando usuario ja existente e salvando o id nos cookies

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../../lib/prisma'
+import {setCookie} from 'nookies'
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,14 +11,28 @@ export default async function handler(
         return res.status(405).end()
     }
 
-
     const {name, username} = req.body 
+
+    const userExistis = await prisma.user.findUnique({
+        where:{
+            username
+        }
+    })
+
+    if(userExistis){
+        return res.status(400).json({message: 'Username already taken'})
+    }
 
     const user = await prisma.user.create({
         data:{
             name, 
             username,
         }
+    })
+
+    setCookie({res}, '@ignitecall:userId', user.id, {
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+        path: '/'
     })
 
     return res.status(201).json(user)
